71e66890fae8c28660a91362f5b7b7e2cdb11321
nuzcraft <nuzcraft@gmail.com>
2022-08-25 13:09:25 -0500

    finished episode 12, checkpoints

--- World.gd
+++ World.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
+const PlayerScene = preload("res://Player.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var player_spawn_location = Vector2.ZERO
+
+onready var shakeCamera := $ShakeCamera
+onready var player := $Player
+onready var timer := $Timer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	VisualServer.set_default_clear_color(Color.skyblue)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	player.connectCamera(shakeCamera)
+	Events.connect("player_died", self, "_on_player_died")
+	Events.connect("hit_checkpoint", self, "_on_hit_checkpoint")
+	player_spawn_location = player.global_position
+	
+func _on_player_died():
+	timer.start(1.0)
+	yield(timer, "timeout")
+	var player = PlayerScene.instance()
+	player.position = player_spawn_location
+	add_child(player)
+	player.connectCamera(shakeCamera)
+	
+func _on_hit_checkpoint(checkpoint_position):
+	player_spawn_location = checkpoint_position
